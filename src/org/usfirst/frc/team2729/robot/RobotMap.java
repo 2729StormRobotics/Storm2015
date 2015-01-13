c744f904ff900217e33373f7c7ce6b960c8fbefe
Tim Gordon(creativeNameHere) <tgordon555@comcast.net>
2015-01-13 17:24:05 -0500

    Changed up more constants, mostly elevator

--- src/org/usfirst/frc/team2729/robot/RobotMap.java
+++ src/org/usfirst/frc/team2729/robot/RobotMap.java
@@ -15,19 +15,20 @@ public class RobotMap {
 	//PWM Ports 
 	public static final int PORT_MOTOR_DRIVE_CENTER	  = 0,
 			PORT_MOTOR_DRIVE_LEFT     = 1,
-			PORT_MOTOR_DRIVE_RIGHT    = 2;
+			PORT_MOTOR_DRIVE_RIGHT    = 2,
+			PORT_MOTOR_ELEVATOR_1	  = 3,
+			PORT_MOTOR_ELEVATOR_2	  = 4;
 	
 	//Digital I/O Ports
 	public static final int PORT_ENCODER_CENTER_1		=  0,
 			PORT_ENCODER_CENTER_2	=  1,
-			PORT_ENCODER_RIGHT_1_1    =  2,
-			PORT_ENCODER_RIGHT_1_2    =  3,
-			PORT_ENCODER_RIGHT_2_1    =  4,
-			PORT_ENCODER_RIGHT_2_2    =  5,
-			PORT_ENCODER_LEFT_1_1     =  6,
-			PORT_ENCODER_LEFT_1_2     =  7,
-			PORT_ENCODER_LEFT_2_1     =  6,
-			PORT_ENCODER_LEFT_2_2     =  7;
+			PORT_ENCODER_RIGHT_1    =  2,
+			PORT_ENCODER_RIGHT_2    =  3,
+			PORT_ENCODER_LEFT_1     =  4,
+			PORT_ENCODER_LEFT_2     =  5,
+			PORT_ENCODER_ELEVATOR_1	  =  6,
+			PORT_ENCODER_ELEVATOR_2   =  7;
+			
 	
 	//Relay
 	public static final int PORT_RELAY_COMPRESSOR = 0;

f41bf9a629fc18093c5e7aecfd27b3851e2aaf54
Justin Becker <Geekman7473@gmail.com>
2015-01-14 17:19:51 -0500

    gradientDrive implemented

--- src/org/usfirst/frc/team2729/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2729/robot/subsystems/DriveTrain.java
@@ -12,9 +12,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class DriveTrain extends Subsystem {
+	//Frame of robot reference.
+	//Theta is in degrees
+	//Theta = 0 is at the front of the robot
+	//Theta increases clockwise
+	//Y axis is parallel to the sides of the robot.
+	//Positive Y is towards the front
+	//X axis is parallel to the front of the robot
+	//Positive X is towards the right
 	
 	private final Talon _left = new Talon(RobotMap.PORT_MOTOR_DRIVE_LEFT),
-					   	_right= new Talon(RobotMap.PORT_MOTOR_DRIVE_RIGHT);
+					   	_right= new Talon(RobotMap.PORT_MOTOR_DRIVE_RIGHT),
+					   	_center=new Talon(RobotMap.PORT_MOTOR_DRIVE_CENTER);
 	
 	private final RobotDrive _drive = new RobotDrive(_left,_right);
 	
@@ -31,6 +40,48 @@ public class DriveTrain extends Subsystem {
 	private boolean _isHighGear = false;
 	
 	private boolean _reverseDrive = false;
+	
+	private final double vertRatioLow = .8;
+	private final double vertRatioHigh = 1.4;
+	private final double LoGTanCoef = Math.tan(4 * vertRatioLow);
+	private final double HiGTanCoef = Math.tan(4 * vertRatioHigh);
+	
+	public void gradientDrive(double mag, double theta, boolean isHighGear){
+		while(theta >= 360.0){ //ensures that no theta exceeds 360 degrees
+			theta-= 360.0;
+		}
+		double curTanCoef = 0;
+		double curRatio = 0;
+		if(isHighGear){
+			curTanCoef = HiGTanCoef;
+			curRatio = vertRatioHigh;
+		} else {
+			curTanCoef = LoGTanCoef; 
+			curRatio = vertRatioLow;
+		}
+		//checks to see if theta is on the interval [0, Coef]U[180-Coef,180+Coef]U[360 - coef, 360]
+		if(theta > (360.0 - curTanCoef) || theta < curTanCoef || (theta < 180 + curTanCoef && theta > 180 - curTanCoef)){ 
+			_center.set((4*curRatio)*Math.tan(curTanCoef));
+			if(theta > 270 || theta < 90){
+				_left.set(1.0 * mag);
+				_right.set(1.0 * mag);
+			} else {
+				_left.set(-1.0 * mag);
+				_right.set(-1.0 * mag);
+			}
+		} else {
+			if(theta != 90 || theta != 270){
+			_right.set(((theta > 180 ? -1 : 1)/(Math.tan(theta)*4*curRatio))*mag);
+			_left.set(((theta > 180 ? -1 : 1)/(Math.tan(theta)*4*curRatio))*mag);
+			_center.set((theta > 180 ? -1 : 1)*mag);
+			} else {
+			_right.set(0);
+			_left.set(0);
+			_center.set((theta > 180 ? -1 : 1)*mag);
+			}
+		}
+		
+	}
 	
 	public DriveTrain(){
 		//Encoders are started when they are initialized

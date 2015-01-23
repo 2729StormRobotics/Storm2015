2739ef25a52518fd7342b959bec9ca767eeae886
Taylor He <Taylor.he7@gmail.com>
2015-01-23 15:46:05 -0500

    Switch in the rake

---
+++ src/org/usfirst/frc/team2729/robot/subsystems/Switch.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team2729.robot.subsystems;
+
+public class Switch {
+
+}
--- src/org/usfirst/frc/team2729/robot/subsystems/rakeArm.java
+++ src/org/usfirst/frc/team2729/robot/subsystems/rakeArm.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team2729.robot.subsystems;
 import org.usfirst.frc.team2729.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -9,7 +11,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 public class rakeArm extends Subsystem{
 	private final Talon _arm = new Talon(RobotMap.PORT_MOTOR_ARM);
 	private final Encoder _armEncoder =  new Encoder(RobotMap.PORT_ENCODER_ARM_1, RobotMap.PORT_ENCODER_ARM_2);
-	
+	private final DigitalInput _switch = new DigitalInput(PORT_LIMIT_SWITCH);
 	public rakeArm(){
 		LiveWindow.addActuator("Arm1", "Arm", _arm);
 		LiveWindow.addSensor("Arm1", "Arm Encoder", _armEncoder);
@@ -17,7 +19,9 @@ public class rakeArm extends Subsystem{
 	public Encoder get_ArmEncoder(){
 		return _armEncoder;
 	}
-	
+	public boolean isPressed(){
+		return _switch.get();
+	}
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub

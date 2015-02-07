dc97b0ded76b6b356fd5d665f39ffadccab4f088
Taylor He <Taylor.he7@gmail.com>
2015-02-07 11:43:13 -0500

    Tote Aligns WIP

---
+++ src/org/usfirst/frc/team2729/robot/commands/auto/LiftYellowStackMove.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team2729.robot.commands.auto;
+
+import org.usfirst.frc.team2729.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class LiftYellowStackMove extends CommandGroup{
+	public LiftYellowStackMove(){
+		Robot
+	}
+}

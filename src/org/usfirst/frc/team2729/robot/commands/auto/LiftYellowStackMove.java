5299a6f76ff8d833b7b96922fc8d7d4657a2470a
Garrett Beatty <gman9732@gmail.com>
2015-02-07 13:28:03 -0500

    added auto stuff

--- src/org/usfirst/frc/team2729/robot/commands/auto/LiftYellowStackMove.java
+++ src/org/usfirst/frc/team2729/robot/commands/auto/LiftYellowStackMove.java
@@ -6,15 +6,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class LiftYellowStackMove extends CommandGroup{
 	public LiftYellowStackMove(){
-		addSequential(new DriveVector(0,20,1));//moves forward a little
+		addSequential(new DriveVector(0,false,20,1));//moves forward a little
 		addSequential(new Stack());//picks up tote
-		addSequential(new DriveVector(0,-20,1));//moves back a little
+		addSequential(new DriveVector(0,false,-20,1));//moves back a little
 		addSequential(new DriveVector(100, 0, 1));//moves to right
 		addSequential(new BinAlignHor(.5)); //aligns
 		addSequential(new BinAlignDepth(.5)); //aligns
-		addSequential(new DriveVector(0,20,1));//moves forward a little
+		addSequential(new DriveVector(0,false, 20,1));//moves forward a little
 		addSequential(new Stack()); //repeat
-		addSequential(new DriveVector(0,-20,1));//moves back a little
+		addSequential(new DriveVector(0,false, -20,1));//moves back a little
 		addSequential(new DriveVector(100, 0, 1));
 		addSequential(new BinAlignHor(.5));
 		addSequential(new BinAlignDepth(.5));

8216b62d1d9d4a2148555283d80c5af36a220155
GarrettBeatty <gman9732@gmail.com>
2015-02-08 17:30:04 -0500

    changed alignment for tote auto

--- src/org/usfirst/frc/team2729/robot/autoModes/OneToteAuto.java
+++ src/org/usfirst/frc/team2729/robot/autoModes/OneToteAuto.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team2729.robot.commands.LowerArmsToReleasePos;
 import org.usfirst.frc.team2729.robot.commands.Stack;
 import org.usfirst.frc.team2729.robot.commands.Unclamp;
 import org.usfirst.frc.team2729.robot.commands.auto.DriveVector;
+import org.usfirst.frc.team2729.robot.commands.auto.ToteAlignHor;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -13,6 +14,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class OneToteAuto extends CommandGroup {
 
 	public OneToteAuto() {
+		addSequential(new ToteAlignHor(.5));
 		addSequential(new DriveVector(0, false, 20, 1));
 		addSequential(new Stack());
 		addSequential(new MoveToAuto());
--- src/org/usfirst/frc/team2729/robot/autoModes/ThreeToteAuto.java
+++ src/org/usfirst/frc/team2729/robot/autoModes/ThreeToteAuto.java
@@ -1,26 +1,28 @@
 package org.usfirst.frc.team2729.robot.autoModes;
 
 import org.usfirst.frc.team2729.robot.commands.Stack;
-import org.usfirst.frc.team2729.robot.commands.auto.BinAlignDepth;
-import org.usfirst.frc.team2729.robot.commands.auto.BinAlignHor;
 import org.usfirst.frc.team2729.robot.commands.auto.DriveVector;
+import org.usfirst.frc.team2729.robot.commands.auto.ToteAlignDepth;
+import org.usfirst.frc.team2729.robot.commands.auto.ToteAlignHor;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class ThreeToteAuto extends CommandGroup{
 	public ThreeToteAuto(){
+		addSequential(new ToteAlignHor(.5)); //aligns
+		addSequential(new ToteAlignDepth(.5)); //aligns
 		addSequential(new DriveVector(0,false,20,1));//moves forward a little
 		addSequential(new Stack());//picks up tote
 		addSequential(new DriveVector(0,false,-20,1));//moves back a little
 		addSequential(new DriveVector(100, 0, 1));//moves to right
-		addSequential(new BinAlignHor(.5)); //aligns
-		addSequential(new BinAlignDepth(.5)); //aligns
+		addSequential(new ToteAlignHor(.5)); //aligns
+		addSequential(new ToteAlignDepth(.5)); //aligns
 		addSequential(new DriveVector(0,false, 20,1));//moves forward a little
 		addSequential(new Stack()); //repeat
 		addSequential(new DriveVector(0,false, -20,1));//moves back a little
-		addSequential(new DriveVector(100, 0, 1));
-		addSequential(new BinAlignHor(.5));
-		addSequential(new BinAlignDepth(.5));
+		addSequential(new DriveVector(100, 0, 1)); //moves to right
+		addSequential(new ToteAlignHor(.5)); //aligns
+		addSequential(new ToteAlignDepth(.5)); //aligns
 		addSequential(new OneToteAuto()); //picks up last tote and goes to auto
 		
 	}

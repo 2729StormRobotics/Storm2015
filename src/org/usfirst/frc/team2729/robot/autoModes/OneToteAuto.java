67d84f37e606f7f28281272c46caab52419e0196
Taylor He <Taylor.he7@gmail.com>
2015-02-13 17:20:28 -0500

    automode stuff

--- src/org/usfirst/frc/team2729/robot/autoModes/OneToteAuto.java
+++ src/org/usfirst/frc/team2729/robot/autoModes/OneToteAuto.java
@@ -1,5 +1,25 @@
 package org.usfirst.frc.team2729.robot.autoModes;
 
-public class OneToteAuto {
+import org.usfirst.frc.team2729.robot.commands.LowerArmsToReleasePos;
+import org.usfirst.frc.team2729.robot.commands.Stack;
+import org.usfirst.frc.team2729.robot.commands.Unclamp;
+import org.usfirst.frc.team2729.robot.commands.auto.DriveVector;
+import org.usfirst.frc.team2729.robot.commands.auto.ToteAlignHor;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class OneToteAuto extends CommandGroup {
+
+public OneToteAuto() {
+addSequential(new ToteAlignHor(.5));
+addSequential(new DriveVector(0, false, 20, 1));
+addSequential(new Stack());
+addSequential(new MoveToAuto());
+addSequential(new LowerArmsToReleasePos());
+addSequential(new Unclamp());
+}
 
 }

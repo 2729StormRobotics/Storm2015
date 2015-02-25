103043c79464a980a58d9f88a36097d4c50dd6db
Justin Becker <geekman7473@gmail.com>
2015-02-24 19:12:05 -0500

    Removed LEDS, end of unbag time

--- src/org/usfirst/frc/team2729/robot/autoModes/OneToteOneContainer.java
+++ src/org/usfirst/frc/team2729/robot/autoModes/OneToteOneContainer.java
@@ -6,12 +6,16 @@ import org.usfirst.frc.team2729.robot.commands.ElevatorClamp;
 import org.usfirst.frc.team2729.robot.commands.Strafe;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class OneToteOneContainer extends CommandGroup{
 	public OneToteOneContainer(){
 		addSequential(new ElevatorClamp(true));
+		addSequential(new WaitCommand(0.5));
 		addSequential(new ChangeElevPosition(1));
-		addSequential(new DriveForward(.7, 500));
-		addSequential(new Strafe(1), 3);
+		addSequential(new ChangeElevPosition(1));
+		addSequential(new WaitCommand(0.25));
+		addSequential(new DriveForward(.7, 400));
+		addSequential(new Strafe(-1), 3);
 	}
 }
